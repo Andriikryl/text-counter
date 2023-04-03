24145f8e36f31b043488afc979f7dd97cb94a8fa
Andriikryl <andreyk557@gmail.com>
2023-04-04 00:09:32 +0300

    feat[count-words]

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ const text = document.querySelector(".js-text");
 const words = document.querySelector(".js-words");
 const characters = document.querySelector(".js-characters");
 const symbols = document.querySelector(".js-symbols");
+
+text.addEventListener("input", () => {
+  words.innerHTML = text.value.trim().split(/\s+/).length;
+});
