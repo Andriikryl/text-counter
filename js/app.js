066dcc4d337aaa51ff84fdd308387e459321ac8b
Andriikryl <andreyk557@gmail.com>
2023-04-04 00:05:35 +0300

    feat[js/prop]

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const text = document.querySelector(".js-text");
+const words = document.querySelector(".js-words");
+const characters = document.querySelector(".js-characters");
+const symbols = document.querySelector(".js-symbols");
